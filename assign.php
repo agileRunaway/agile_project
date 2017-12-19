639205088cd5ebc0d779c226008557a3583c36d3
sam9494 <s102213053@ncnu.edu.tw>
2017-12-19 20:08:28 +0800

    Add files via upload

--- assign.php
+++ assign.php
@@ -1,4 +1,5 @@
 <?php
+/*
 if(isset($_POST["action"])&&($_POST["action"]=="add")){
 	include("connMysql.php");
 	$sql_query = "INSERT INTO `project_data` (`PName` ,`A_member` ,`Startdate` ,`Enddate` ,`Content` ) VALUES (";
@@ -8,75 +9,9 @@ if(isset($_POST["action"])&&($_POST["action"]=="add")){
 	$sql_query .= "'".$_POST["eday"]."',";
 	$sql_query .= "'".$_POST["textbar"]."')";
 	mysqli_query($db_link, $sql_query);
-}
-?>
-<!DOCTYPE html>
-<html>
-<head>
-<title>功能分派</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-* {box-sizing: border-box;}
-body {margin-left:10px;}
-.sidenav {height: 100%;width: 400px;position: fixed;z-index: 1;top: 100;
-    left: 0;background-color: #e6ffff;overflow-x: hidden;padding: 20px;line-height: 30px;}
-.content {margin-left: 400px;}
-table th {background-color: #b3f0ff;}
-table td, th{border-bottom: 1px solid #b3f0ff;padding: 10px;text-align: left;}
-.input-title {float: left;width: 25%;margin-right: 20px;}
-.input-data {float: left;width: 25%;}
-.AlignRight {text-align: right;}
-label{font-weight:bold;}
-</style>
-</head>
-<body>
-<div class="header">
-	<p style="color:#4d79ff; padding-left:40px; font-size"><b>敏捷開發專案</b></p>
-	<h2><img src="contract.png" width="40" height="40"><b style="padding-top: 8px">功能分派
-	<a style="padding-left:100px; font-size:15px" href="data.php">回主畫面</a></b></h2>
-  </div>
-<div class="sidenav">
-	<form action="" method="post" name="formAdd" id="formAdd">
-		<div class="AlignRight">
-			<div class="input-title"> 
-				<label for="PName">名稱:</label> <br/>
-				<label for="YourMember">員工指派:</lable><br/>
-				<label for="sday">開始日期:</label><br/>
-				<label for="eday">截止日期:</label><br/>
-				<label for="textbar">文字說明:</label><br/>
-			</div>
-		</div>
-		<div class="input-data">
-			<input type="text" name="PName"><br>
-			<select name="YourMember">
-			　<option value="陳曉照">陳曉照</option>
-			　<option value="戴郁佳">戴郁佳</option>
-			　<option value="鍾大宣">鍾大宣</option>
-			　<option value="夜來香">夜來香</option>
-			</select>
-			<input type="date" name="sday"><br>
-			<input type="date" name="eday"><br>
-		</div>
-		<div style="padding:20px">
-			<textarea name="textbar" cols="45" rows="10">輸入你想要寫的內容...</textarea>
-			<input name="action" type="hidden" value="add">
-			<input type="submit" name="button" id="button" value="新增">
-		</div>
-	</form>
-</div>
-<div class="content">
-  <table>
-  <tr>
-    <th>名稱</th>
-    <th>員工指派</th>
-    <th>開始日期</th>
-	<th>截止日期</th>
-	<th>文字說明</th>
-  </tr>
-  <?php
-	include("connMysql.php");
-	$sql2 = "SELECT* FROM `project_data` ";
+
+	include("conn.php");
+	$sql2 = "SELECT* FROM `project` ";
 	$result = mysqli_query($db_link, $sql2);
 	while($row_result=mysqli_fetch_assoc($result)){
 		echo "<tr>";
@@ -89,8 +24,216 @@ label{font-weight:bold;}
 		echo "<a href='delete.php?id=".$row_result["id"]."'><img src='delete.png'width='50' height='50'</a></td>";
 		echo "</tr>";
 	}
+	*/
+	include("conn.php");
+	//需要的session
+	$_SESSION["pID"]="1";
+	$_SESSION["dep_id"]="106";
+	$_SESSION["state"]="1";
+	$_SESSION["pro_mem_id"]="1";
+	
+	$pro_mem_id=$_SESSION["pro_mem_id"];
+	$pID=$_SESSION["pID"];
+	$state=$_SESSION["state"];
+	$dep_id=$_SESSION["dep_id"];
+	
+	$obj=(int)$_POST['obj'];
+	
+	//init
+	if($obj==0){
+	
+		//顯示step or task
+		//state=1 是pm
+		if($state==1){
+			$sql2 = 'SELECT * FROM `step` where `pro_id` ="' .$pID. '"';
+			
+			$result = mysqli_query($db_link, $sql2);
+
+			while($row_result=mysqli_fetch_assoc($result)){
+				echo "<tr id='step_".$row_result['step_id']."'>";
+				echo("<td>".$row_result['name']."</td>");
+				$sql3='SELECT department.name FROM `step` INNER JOIN `department` ON step.dep_id=department.dep_id where department.dep_id="'.$row_result['dep_id'].'"';
+				$result1 = mysqli_query($db_link, $sql3);
+				$row_result1=mysqli_fetch_assoc($result1);
+				echo("<td>".$row_result1['name']."</td>");
+				echo("<td>".$row_result['s_time']."</td>");
+				echo("<td>".$row_result['e_time']."</td>");
+				echo("<td><input type='button' onclick='delstep(".$row_result['step_id'].");' value='刪除'></td>");
+				echo "</tr>";
+				
+			}
+			//分割字元
+			echo("|&|");
+			
+			$sql='SELECT * FROM `department` where `man_dep` IS NULL';
+			$dep_result = mysqli_query($db_link, $sql);
+			while($row_result=mysqli_fetch_assoc($dep_result)){
+				echo('<option value=dep_"'.$row_result['dep_id'].'">'.$row_result['name'].'</option>');
+			}
+		}
+		// state=2 經理
+		else if($state==2){
+			$sql2 = 'SELECT step_id,name FROM `step` where `pro_id` ="' .$pID. '" AND owner_id ="' .$pro_mem_id. '"';
+			$result = mysqli_query($db_link, $sql2);
+			while($row_result=mysqli_fetch_assoc($result)){
+				//echo($row_result['step_id']);
+				$sql3 = 'SELECT * FROM `task` where `step_id` ="' .$row_result['step_id']. '"';
+				$result1 = mysqli_query($db_link, $sql3);
+				while($row_result1=mysqli_fetch_assoc($result1)){
+					echo "<tr id='task_".$row_result1['task_id']."'>";
+					echo("<td>".$row_result1['name']."</td>");
+					echo("<td>");
+					$sql4 = 'SELECT mem_name FROM `task_mem` where `task_id` ="' .$row_result1['task_id']. '"';
+					$result2 = mysqli_query($db_link, $sql4);
+					while($row_result2=mysqli_fetch_assoc($result2)){
+						echo(" ".$row_result2['mem_name']." ");
+						//$num=mysql_num_rows($result2);
+						//echo(" & ");
+					}
+					echo("</td>");
+					echo("<td>".$row_result1['s_time']."</td>");
+					echo("<td>".$row_result1['e_time']."</td>");
+					echo("<td>".$row_result1['description']."</td>");
+					echo("<td><input type='button' onclick='deltask(".$row_result1['task_id'].");' value='刪除'></td>");
+					echo "</tr>";
+				}
+			}
+			echo("|&|");
+			//找出在該project裡經理負責的step
+			$sql2 = 'SELECT step_id,name FROM `step` where `pro_id` ="' .$pID. '" AND owner_id ="' .$pro_mem_id. '"';
+			$result = mysqli_query($db_link, $sql2);
+			while($row_result=mysqli_fetch_assoc($result)){
+				
+				echo('<option value="step_'.$row_result['step_id'].'">'.$row_result['name'].'</option>');
+			}
+			echo("|&|");
+			
+			//找出這經理底下的員工
+			$sql='SELECT * FROM `member` where `dep_id` = '.$dep_id.'';
+			$mem_result = mysqli_query($db_link, $sql);
+			while($row_result=mysqli_fetch_assoc($mem_result)){
+				echo('<option value="'.$row_result['mem_id'].'_'.$row_result['name'].'">'.$row_result['name'].'</option>');
+			}
+			
+		}
+	}
+	//obj=1 find main_department
+	else if($obj==1){
+		
+		$dep_sel=$_POST['dep_sel'];
+		echo($dep_sel);
+		$sql='SELECT * FROM `department` where `man_dep` = '.$dep_sel.'';
+		$dep_result = mysqli_query($db_link, $sql);
+		while($row_result=mysqli_fetch_assoc($dep_result)){
+			$depid=$row_result['dep_id'];
+			$manager=$row_result['manager'];
+			echo('<option value="'.$row_result['name'].'_'.$depid.'_'.$manager.'">'.$row_result['name'].'</option>');
+		}
+	}
+	//obj=2 insert task
+	else if($obj==2){
+		$PName=$_POST['PName'];
+		$sday=$_POST['sday'];
+		$eday=$_POST['eday'];
+		$text_content=$_POST['text_content'];
+		$memid=$_POST['memid'];
+		$memname=$_POST['memname'];
+		$stepid=$_POST['stepid'];
+		$sql='INSERT INTO `task` (name , s_time , e_time , status , description , step_id) VALUES ("'.$PName.'","'.$sday.'","'.$eday.'","0","'.$text_content.'","'.$stepid.'")';
+		$result = mysqli_query($db_link, $sql);
+		
+		$task_id=mysqli_insert_id($db_link);
+		
+		$sql2='INSERT INTO `task_mem` (task_id , s_time , e_time , status , mem_id , mem_name) VALUES ("'.$task_id.'","'.$sday.'","'.$eday.'","0","'.$memid.'","'.$memname.'")';
+		$result2 = mysqli_query($db_link, $sql2);
+		echo($result2);
+		echo("|&|");
+		echo($task_id);
+		
+	}
+	//obj=3 insert step
+	else if($obj==3){
+		$PName=$_POST['PName'];
+		$sday=$_POST['sday'];
+		$eday=$_POST['eday'];
+		$text_content=$_POST['text_content'];
+		$depid=$_POST['depid'];
+		$manager=$_POST['manager'];
+		$sql1='INSERT INTO `project_member` (pro_id , mem_id  , s_time , e_time , status , Permission) VALUES ("'.$pID.'" ,"'.$manager.'" , "'.$sday.'","'.$eday.'","0","2")';
+		$result1 = mysqli_query($db_link, $sql1);
+		$sql='INSERT INTO `step` (owner_id , name , dep_id , s_time , e_time , pro_id , seq) VALUES ("'.$manager.'" ,"'.$PName.'" , '.$depid.',"'.$sday.'","'.$eday.'","'.$pID.'","1")';
+		$result = mysqli_query($db_link, $sql);
+		echo($result);
+		$step_id=mysqli_insert_id($db_link);
+		echo("|&|");
+		echo($step_id);
+	}
+	//delete task
+	else if($obj==4){
+		$task_id=$_POST['task_id'];
+		$sql='DELETE FROM task WHERE task_id = '.$task_id.'';
+		$result = mysqli_query($db_link, $sql);
+		$sql1='DELETE FROM task_mem WHERE task_id = '.$task_id.'';
+		$result1 = mysqli_query($db_link, $sql1);
+		echo($result1);
+		
+	}
+	//delete step
+	else if($obj==5){
+		$step_id=$_POST['step_id'];
+		$sql='SELECT * FROM task where step_id ='.$step_id.'';
+		$result = mysqli_query($db_link, $sql);
+		
+		$num_rows = mysqli_num_rows($result);
+		if($num_rows==0){
+			$sql1='DELETE FROM step WHERE step_id = '.$step_id.'';
+			$result1 = mysqli_query($db_link, $sql1);
+			echo($result1);
+		}
+		else{
+			echo('0');
+		}
+		/*
+		while($row_result=mysqli_fetch_assoc($result)){
+			echo($row_result['name']);
+		}
+		/*
+		$sql='DELETE FROM step WHERE step_id = '.$step_id.'';
+		$result = mysqli_query($db_link, $sql);
+		
+		$sql1='DELETE FROM task_mem WHERE task_id = '.$task_id.'';
+		$result1 = mysqli_query($db_link, $sql1);
+		echo($result1);
+		*/
+		
+	}
+	else if($obj==6){
+		$sql = 'SELECT * FROM `step` where `pro_id` ="' .$pID. '"';
+		$result = mysqli_query($db_link, $sql);
+		
+		
+		while($row_result=mysqli_fetch_assoc($result)){
+			//echo('{id:'.$row_result['step_id'].' ,name:"'.$row_result['name'].'", series: [');
+			echo($row_result['step_id']);
+			echo("|&$|");
+			echo($row_result['name']);
+			echo("*&*");
+			$sql2 = 'SELECT * FROM `task` where `step_id` ="' .$row_result['step_id']. '"';
+			$result2 = mysqli_query($db_link, $sql2);
+			while($row_result2=mysqli_fetch_assoc($result2)){
+				$stime=str_replace("-",",",$row_result2['s_time']);
+				$etime=str_replace("-",",",$row_result2['e_time']);
+				//echo('{name:"'.$row_result2['name'].'", start:new Date('.$stime.'), end:new Date('.$etime.')},');
+				echo($row_result2['name']);
+				echo("|&$|");
+				echo($stime);
+				echo("|&$|");
+				echo($etime);
+				echo("*&*");
+			}
+			echo("|&|");
+		}
+		
+	}
   ?>
-  <table>
-</div>
-</body>
-</html>
+
